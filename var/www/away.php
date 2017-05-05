84a5ecd5d633c066b4480f070d7cbd41f0e90bce
paulpoco <paulpoco@gmail.com>
2017-05-04 18:39:01 -0700

    Update away.php
    I added reactive updates for mag switch and PIR sensor

--- var/www/away.php
+++ var/www/away.php
@@ -1,43 +1,44 @@
-
 <?php
         if(isset($_GET['trigger']) && $_GET['trigger'] == 1) {
                 error_reporting(E_ALL);
-                exec('gpio write 0 0'); // Output GPIO 0 as 0  replace with what you use
+                exec('gpio write 0 0');
                 usleep(1000000);
-                exec('gpio write 0 1'); // Output GPIO 0 as 1  replace with what you use
+                exec('gpio write 0 1');
         }
-exec ( "gpio read 2", $status ); // Read GPIO 2 connected to mag reed switch  replace with what you use
-
-if ( $status[0] == 0 ) {
 ?>
-<h1><mark>Door Closed</mark></h1>
-<?php
-} else {
-?>
-<h1><mark>Door Open</mark></h1><p>
-<?php
-}
-?>
-
-<!DOCTYPE html>
 <html>
+<div id="serverData">Here is where the server sent data will appear</div>
+<script type="text/javascript">
+//functions here
+//check for browser support
+if(typeof(EventSource)!=="undefined") {
+        //create an object, passing it the name and location of the server side script
+        var eSource = new EventSource("send_door_status_away.php");
+        //detect message receipt
+        eSource.onmessage = function(event) {
+                //write the received data to the page
+                document.getElementById("serverData").innerHTML = event.data;
+        };
+}
+else {
+        document.getElementById("serverData").innerHTML="Whoops! Your browser doesn't receive server-sent events.";
+}
+</script>
         <head>
-                <title>Garage Opener</title>
                 <link rel="apple-touch-icon" href="apple-touch-icon-iphone.png" />
                 <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-ipad.png" />
                 <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-iphone-retina-display.png" />
                 <link rel="stylesheet" href="/css/style.css" type="text/css">
-                <meta name="apple-mobile-web-app-capable" content="yes">        
-                <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>    
-                <script type="text/javascript" src="/js/script.js"></script>    
-
+                <meta name="apple-mobile-web-app-capable" content="yes">
+                <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+                <script type="text/javascript" src="/js/script.js"></script>
         </head>
         <body>
                 <!--Replace with your own, port forward in router forwardport to 8081-->
                 <!--Replace with your DDNS hostname to DDNSip-->
                 <img style="-webkit-user-select: none" src="http://DDNSip:forwardport/" width="980" height="735">
                 <div class='awrap'>
-                <a href='/?trigger=1'></a>
+              <a href='/?trigger=1'></a>
                 </div>
         </body>
 </html>
