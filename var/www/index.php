e80e2f4be9ad5894d90910cb1daa933caf8468c2
paulpoco <paulpoco@gmail.com>
2017-05-04 18:42:03 -0700

    Update index.php

--- var/www/index.php
+++ var/www/index.php
@@ -5,31 +5,33 @@
                 usleep(1000000);
                 exec('gpio write 0 1'); // Output GPIO 0 as 1  replace with what you use
         }
-exec ( "gpio read 2", $status ); // Read GPIO 2 connected to mag reed switch  replace with what you use
-
-if ( $status[0] == 0 ) {
 ?>
-<h1><mark>Door Closed</mark></h1>
-<?php
-} else {
-?>
-<h1><mark>Door Open</mark></h1><p>
-<?php
-}
-?>
-
-<!DOCTYPE html>
 <html>
+<div id="serverData">Here is where the server sent data will appear</div>
+<script type="text/javascript">
+//functions here
+//check for browser support
+if(typeof(EventSource)!=="undefined") {
+        //create an object, passing it the name and location of the server side script
+        var eSource = new EventSource("send_door_status.php");
+        //detect message receipt
+        eSource.onmessage = function(event) {
+                //write the received data to the page
+                document.getElementById("serverData").innerHTML = event.data;
+        };
+}
+else {
+        document.getElementById("serverData").innerHTML="Whoops! Your browser doesn't receive server-sent events.";
+}
+</script>
         <head>
-                <title>Garage Opener</title>
-                <link rel="apple-touch-icon" href="apple-touch-icon-iphone.png" />
+		<link rel="apple-touch-icon" href="apple-touch-icon-iphone.png" />
                 <link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-ipad.png" />
                 <link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-iphone-retina-display.png" />
                 <link rel="stylesheet" href="/css/style.css" type="text/css">
-                <meta name="apple-mobile-web-app-capable" content="yes">        
-                <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>    
-                <script type="text/javascript" src="/js/script.js"></script>    
-
+                <meta name="apple-mobile-web-app-capable" content="yes">
+                <script type="text/javascript" src="/js/jquery-1.10.2.min.js"></script>
+                <script type="text/javascript" src="/js/script.js"></script>
         </head>
         <body>
                 <!--Replace RaspiIP with your IP-->
